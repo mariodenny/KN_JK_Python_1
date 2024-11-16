10b0d5958493cacdafa40c939ae5170ec10b8222
ITsDenny <dennym.channel@gmail.com>
2024-11-16 17:12:26 +0700

    feat(lesson1):tambahan penjelasan modulo

--- lesson1/arithmethic.py
+++ lesson1/arithmethic.py
@@ -19,3 +19,39 @@ print(my_number/my_second_number)
 print(my_number**my_second_number)
 print(my_number%my_second_number)
 
+
+"""
+Penjelasan sederhana mengenai modulo / modulus / sisa bagi
+misal ada soal gini
+12%3 = ? 
+12 mod 3 berapa sih? cari tau caranya
+pertama bagi langsung 
+12 / 3 = 4
+dapat hasilnya 4.
+untuk mendapatkan sisa bagi, cukup kalikan hasil bagi dengan pembagi
+hasil bagi = 4
+pembagi = 3
+4*3 = 12
+kurangkan hasil perkalian dengan angka di yang dicari sisa baginya,
+
+hasil perkalian = 12
+modulo yang dicari = 12
+12 - 12 = 0
+maka sisa bagi adalah 0
+jadi
+12%3 = 0
+misal ada contoh soal modulo dengan sisa bagi
+14%3 = ?
+langsung saja
+14 / 3 = 4,66 -> bulatkan jadi angka depan saja 
+14/3 = 4
+pembuktian sisa bagi
+hasil bagi = 4
+pembagi = 3
+4*3 = 12
+cari sisa bagi
+14 - 12 = 2
+maka 
+14 mod 3 = 2
+
+"""
